98f8c53d025f06c405697c066e9154fbd3ca6881
seeba <poczta@sebastianpluta.pl>
2019-04-16 11:13:54 +0200

    update commit

---
+++ src/Form/NewsType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\News;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NewsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+            ->add('publishAt')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('enabled')
+            ->add('image', FileType::class, ['label' => 'Wybierz zdjęcie'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => News::class,
+        ]);
+    }
+}
